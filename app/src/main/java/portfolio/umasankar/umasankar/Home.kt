1380bfbea870d70776709fb38f70264b4eaca4a6
Umasankar <umasankar@prandex.com>
2018-05-16 15:12:47 +0530

    App color modification

--- app/src/main/java/portfolio/umasankar/umasankar/Home.kt
+++ app/src/main/java/portfolio/umasankar/umasankar/Home.kt
@@ -2,11 +2,14 @@ package portfolio.umasankar.umasankar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        getSupportActionBar()?.setTitle((Html.fromHtml("<font color=\"#ffffff\">" + "Test" + "</font>")));
+
     }
 }
